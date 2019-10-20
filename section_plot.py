bdf1885cfdc4f38cf172258f9735eef1e168ac98
manuel <jose.m.garcia1931@gmail.com>
2019-10-20 14:46:59 -0500

    new update for section analysis tools

--- section_plot.py
+++ section_plot.py
@@ -3,10 +3,11 @@ import numpy as np
 from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
 import pandas as pd
+import json
 
 def initializeAnalysis(shapes):
 
-    def section_properties(test):
+    def section_properties(test, mirror=False, xbar_init=0, ybar_init=0):
         columns = ["el", "base", "height", "Area", "xbar", "Ax", "Ax^2", "Ioy"]
         data_y = pd.DataFrame(columns=columns)
 
@@ -41,10 +42,17 @@ def initializeAnalysis(shapes):
         for i in range(len(test)):
             basex = test[i][1]
             heightx = test[i][2]
+            if(abs(ybar_init - (test[i][0][1] + heightx)) < 0.01 or ybar_init > test[i][0][1] + heightx):
+                continue
             areax = basex * heightx
-            ybarx = test[i][0][1] + heightx / 2
-            Ay_x = areax * ybarx
-            Ay_sqx = Ay_x * ybarx
+            if(mirror==True):
+                ybarx = (test[i][0][1] + heightx / 2) - ybar_init
+                Ay_x = areax * ybarx
+                Ay_sqx = Ay_x * ybarx
+            else:
+                ybarx = test[i][0][1] + heightx / 2
+                Ay_x = areax * ybarx
+                Ay_sqx = Ay_x * ybarx
             Iox = (basex * heightx**3) / 12
 
             data_x.at[i, "el"] = i + 1
@@ -68,25 +76,48 @@ def initializeAnalysis(shapes):
         # print("Ay      = {:.4f}".format(sums_x["Ay"]))
         # print("Ax      = {:.4f}".format(sums_y["Ax"]))
 
+        if(mirror==True):
+            return sums_x["Ay"], sums_y["Ax"]
 
-        return Iox_all, Ioy_all, xbar_cg, ybar_cg, sums_x["Ay"], sums_y["Ax"]
+        return Iox_all, Ioy_all, xbar_cg, ybar_cg, sums_x["Ay"], sums_y["Ax"], sums_x["Area"], sums_y["Area"]
 
     # shapes is and array of [ [lower left corner, base, height] ]
-    def plot_shapes(shapes):
-        Iox_all, Ioy_all, xbar_cg, ybar_cg, Ay, Ax = section_properties(shapes)
+    def plot_shapes(shapes, mirror=False):
+        Iox_all, Ioy_all, xbar_cg, ybar_cg, Ay, Ax, Area_x, Area_y = section_properties(shapes)
 
-        fig, ax = plt.subplots(1)
-        patches = []
+        if(mirror==True):
+            Ay, Ax = section_properties(shapes, mirror=mirror, xbar_init=xbar_cg, ybar_init=ybar_cg)
 
-        max_x = .5
-        max_y = .5
+        max_x = 0.0
+        max_y = 0.0
+        min_x = 9999.0
+        min_y = 9999.0
 
         for i in shapes:
-            rect = Rectangle( i[0], i[1], i[2])
             if (i[0][0] + i[1]) > max_x:
                 max_x = (i[0][0] + i[1])
             if (i[0][1] + i[2]) > max_y:
                 max_y = (i[0][1] + i[2])
+            if (i[0][0]) < min_x:
+                min_x = (i[0][0])
+            if (i[0][1]) < min_y:
+                min_y = (i[0][1])
+
+        first_moment_y = 2*Ay*(max_y-min_y)*.5 / Iox_all
+
+        fig, ax = plt.subplots(1)
+        patches = []
+
+        for i in shapes:
+            rect = Rectangle( i[0], i[1], i[2])
+            # if (i[0][0] + i[1]) > max_x:
+            #     max_x = (i[0][0] + i[1])
+            # if (i[0][1] + i[2]) > max_y:
+            #     max_y = (i[0][1] + i[2])
+            # if (i[0][0]) < min_x:
+            #     max_x = (i[0][0])
+            # if (i[0][1]) < min_y:
+            #     max_y = (i[0][1])
             patches.append(rect)
 
         ax.axhline(y=ybar_cg, color='r')
@@ -96,7 +127,7 @@ def initializeAnalysis(shapes):
         ax.add_collection(p)
         ax.set_xlim([0, max_x * 1.3])
         ax.set_ylim([0, max_y * 1.3])
-        # plt.show()
+        plt.show()
         # html_fig = mpld3.fig_to_html(fig=fig)
         html_fig = "holder"
 
@@ -137,7 +168,19 @@ def initializeAnalysis(shapes):
                 new_height = C_divider - y_orig
                 lower.append([new_origin, new_base, new_height])
 
-        return upper, lower
+        Iox_all_up, Ioy_all_up, xbar_cg_up, ybar_cg_up, Ay_up, Ax_up, Area_x_up, Area_y_up = section_properties(upper)
+
+        Iox_all_low, Ioy_all_low, xbar_cg_low, ybar_cg_low, Ay_low, Ax_low, Area_x_low, Area_y_low = section_properties(lower)
+
+        tollerance = abs(Area_x_up - Area_x_low)
+        if(tollerance < 0.005):
+            return upper, lower, C_divider
+        if( (Area_x_up - Area_x_low) > 0.005 and (Area_x_up - Area_x_low) > 0 ):
+            upper, lower, C_divider = split_shape(test, C_divider + 0.01)
+        elif ( (Area_x_low - Area_x_up) > 0.005 and (Area_x_up - Area_x_low) > 0 ):
+            upper, lower, C_divider = split_shape(test, C_divider - 0.01)
+
+        return upper, lower, C_divider
 
     def upper_mirror(upper, C_divider):
         # x stays the same
@@ -173,35 +216,31 @@ def initializeAnalysis(shapes):
             lower.append(i)
         return lower
 
-    # test = [[(0.50,    0), 1.00, 0.30],
-    #         [(0.85,  0.3), 0.30, 2.40],
-    #         [(0   ,  2.7), 2.00, 0.30]]
-
-    # print("********************************")
-    # print("Original Shape")
+    print("********************************")
+    print("Original Shape")
     originalShapeProps, htmlOriginal = plot_shapes(shapes)
-    # print("********************************")
+    print("********************************")
 
     #Split Shapes
-    upper, lower = split_shape(shapes, 2)
+    upper, lower, C_divider = split_shape(shapes, 0)
 
     # print("Upper Shape")
-    upperShapeProps, htmlUpper = plot_shapes(list(upper))
+    # upperShapeProps, htmlUpper = plot_shapes(list(upper))
     # print("********************************")
-
+    #
     # print("Lower Shape")
-    lowerShapeProps, htmlLower = plot_shapes(list(lower))
+    # lowerShapeProps, htmlLower = plot_shapes(list(lower))
     # print("********************************")
 
-    # print("Upper Mirrored Shape")
-    upper_mirrored = upper_mirror(list(upper), 2)
-    upperMirroredProps, htmlUpperMirrored = plot_shapes(upper_mirrored)
-    # print("********************************")
+    print("Upper Mirrored Shape")
+    upper_mirrored = upper_mirror(list(upper), C_divider)
+    upperMirroredProps, htmlUpperMirrored = plot_shapes(upper_mirrored, mirror=True)
+    print("********************************")
 
-    # print("Lower Mirrored Shape")
-    lower_mirrored = lower_mirror(list(lower), 2)
-    lowerMirroredProps, htmlLowerMirrored = plot_shapes(lower_mirrored)
-    # print("********************************")
+    print("Lower Mirrored Shape")
+    lower_mirrored = lower_mirror(list(lower), C_divider)
+    lowerMirroredProps, htmlLowerMirrored = plot_shapes(lower_mirrored, mirror=True)
+    print("********************************")
 
     result = { "analysis": {
         "OriginalShape": {
@@ -210,16 +249,16 @@ def initializeAnalysis(shapes):
             "geom": shapes
         },
 
-        "Upper": {
-            "props": upperShapeProps.to_dict(),
-            "html": htmlUpper,
-            "geom": upper
-        },
-        "Lower": {
-            "props": lowerShapeProps.to_dict(),
-            "html": htmlLower,
-            "geom": lower
-        },
+        # "Upper": {
+        #     "props": upperShapeProps.to_dict(),
+        #     "html": htmlUpper,
+        #     "geom": upper
+        # },
+        # "Lower": {
+        #     "props": lowerShapeProps.to_dict(),
+        #     "html": htmlLower,
+        #     "geom": lower
+        # },
         "UpperMirroredProps": {
             "props": upperMirroredProps.to_dict(),
             "html": htmlUpperMirrored,
@@ -231,4 +270,11 @@ def initializeAnalysis(shapes):
             "geom": lower_mirrored
         }
     }}
+    print(json.dumps(result, indent=2))
     return result
+
+test = [[(0.50,    0), 1.00, 0.30],
+        [(0.85,  0.3), 0.30, 2.40],
+        [(0   ,  2.7), 2.00, 0.30]]
+
+initializeAnalysis(test)
